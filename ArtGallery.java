b000906fe7d6747df271b9f9b6f46a630f41eb61
Gummy14 <alex.helm@yahoo.com>
2017-11-08 22:28:23 -0500

    New, Better code

--- ArtGallery.java
+++ ArtGallery.java
@@ -12,24 +12,26 @@ import javax.swing.JPanel;
 import java.awt.Polygon;
 
 public class ArtGallery extends JPanel implements ActionListener{
-	//Variables
 	String x;
 	String y;
-	int test = 18;
+	int test = 23;
 	
 	double distance = 0;
+	double ang = 0;
+	double slope = 0;
 	double midx = 0;
 	double midy = 0;
 	int size = 26;
 	int count = 0;
 	int setx[] = new int[size];
 	int sety[] = new int[size];
+	double endx = 0;
+	double endy = 0;
+	double length = 1;
 	int sightx[] = new int[size];
 	int sighty[] = new int[size];
 	Polygon p;
 	boolean intersect = false;
-	
-	//Reads in the stuff from the gallery file
 	public void readFile() throws IOException
 	{
 		FileReader opnfile = new FileReader("Gallery.txt");
@@ -66,8 +68,6 @@ public class ArtGallery extends JPanel implements ActionListener{
 		//count  = 0;
 		double xtest = setx[test]*5;
 		double ytest = sety[test]*5;
-		
-		//This code segment is currently in time-out
 		//Every vertex can see the other two it's connected to
 		//get the next one
 		/*if(test != 25)
@@ -93,9 +93,7 @@ public class ArtGallery extends JPanel implements ActionListener{
 			sighty[1] = sety[25];
 		}
 		count=2;*/
-		
 		super.paintComponent(g);
-		//draw all the vertecies
 		for(int i = 0; i<setx.length;i++)
 		{
 			setx[i] = setx[i]*5;
@@ -107,322 +105,21 @@ public class ArtGallery extends JPanel implements ActionListener{
 		g.drawPolygon(p);
 		//g.fillPolygon(p);
 		g.setColor(Color.RED);
-		
-		//compare every other vertex with the one we're looking at to see what it can see
-		for(int i = 0; i<setx.length;i++)
+		ang = 0;
+		while(ang <=360)
 		{
-			
-			//check every edge to see if there's any intersecting
-			// if so, don't include that edge
-			for(int h = 0; h<setx.length-1;h++)
+			length=1;
+			endx = setx[test] + length*Math.cos(ang);
+			endy = sety[test] + length*Math.sin(ang);
+			while(p.contains(endx,endy) == true)
 			{
-				if(h != i && h+1 != i)
-				{
-					//The intersection function i found wasn't working exactly how i wanted it to so this code adjusts for that
-					
-					//for right angles
-					if(p.contains(xtest+1, ytest-1) == false && p.contains(xtest-1, ytest+1) == true)
-					{
-						intersect = Line2D.linesIntersect(xtest-1,ytest+1,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-					}
-					else if(p.contains(xtest+1, ytest+1) == false && p.contains(xtest-1, ytest-1) == true)
-					{
-						intersect = Line2D.linesIntersect(xtest-1,ytest-1,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-					}
-					else if(p.contains(xtest-1, ytest+1) == false && p.contains(xtest+1, ytest-1) == true)
-					{
-						intersect = Line2D.linesIntersect(xtest+1,ytest-1,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-					}
-					else if(p.contains(xtest-1, ytest-1) == false && p.contains(xtest+1, ytest+1) == true)
-					{
-						intersect = Line2D.linesIntersect(xtest+1,ytest+1,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-					}
-					else
-					{
-						//used for non-right angles
-						//basically it takes the mid point and uses that to place the point
-						getMid(setx[test-1],sety[test-1], setx[test+1],sety[test+1]);
-						//g.drawLine((((int)setx[test])), (((int)sety[test])), (((int)midx)),(((int)midy)));
-						
-						//for exterior acute angles
-						//jutting out of the museum
-						if(p.contains(midx,midy))
-						{
-							//double slope = (midy-ytest)/(midx-xtest);
-							if(midx>xtest && midy>ytest)
-							{
-								while(Math.abs(midx-xtest)>10 || Math.abs(midy-ytest)>10)
-								{
-								while(true)
-								{
-									if(p.contains(midx-1, midy))
-									{
-										midx=midx-1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								while(true)
-								{
-									if(p.contains(midx, midy-1))
-									{
-										midy=midy-1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								}
-							}
-							else if(midx>xtest && midy<ytest)
-							{
-								while(Math.abs(midx-xtest)>10 || Math.abs(midy-ytest)>10)
-								{
-								while(true)
-								{
-									if(p.contains(midx-1, midy))
-									{
-										midx=midx-1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								while(true)
-								{
-									if(p.contains(midx, midy+1))
-									{
-										midy=midy+1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								}
-							}
-							else if(midx<xtest && midy<ytest)
-							{	
-								while(Math.abs(midx-xtest)>10 || Math.abs(midy-ytest)>10)
-								{
-								while(true)
-								{
-									if(p.contains(midx+1, midy))
-									{
-										midx=midx+1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								while(true)
-								{
-									if(p.contains(midx, midy+1))
-									{
-										midy=midy+1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								}
-								//g.drawLine((((int)setx[test])), (((int)sety[test])), (((int)midx)),(((int)midy)));
-							}
-							else if(midx<xtest && midy>ytest)
-							{
-								while(Math.abs(midx-xtest)>10 || Math.abs(midy-ytest)>10)
-								{
-								while(true)
-								{
-									if(p.contains(midx+1, midy))
-									{
-										midx=midx+1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								while(true)
-								{
-									if(p.contains(midx, midy-1))
-									{
-										midy=midy-1;
-									}
-									else
-									{
-										break;
-									}
-								}
-								}
-							}
-							
-							//xtest = midx;
-							//ytest = midy;
-							intersect = Line2D.linesIntersect(midx,midy,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-							
-						}
-						else
-						{
-							//for interior acute angles
-							if(i != test-1 || i != test+1)
-							{
-								if(midx>xtest && midy>ytest)
-								{
-									while(Math.abs(midx-xtest)>2 || Math.abs(midy-ytest)>2)
-									{
-									while(true)
-									{
-										if(p.contains(midx, midy-1) == false)
-										{
-											midy=midy-1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-										if(p.contains(midx-1, midy)  == false)
-										{
-											midx=midx-1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-									//midx=midx-3;
-									midy=midy-3;
-								}
-								else if(midx>xtest && midy<ytest)
-								{
-									while(Math.abs(midx-xtest)>2 || Math.abs(midy-ytest)>2)
-									{
-									while(true)
-									{
-										if(p.contains(midx-1, midy) == false)
-										{
-											midx=midx-1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-										if(p.contains(midx, midy+1)  == false)
-										{
-											midy=midy+1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-									midx=midx-9;
-									midy=midy+5;
-									
-								}
-								else if(midx<xtest && midy<ytest)
-								{									
-									while(Math.abs(midx-xtest)>2 || Math.abs(midy-ytest)>2)
-									{
-									while(true)
-									{
-										if(p.contains(midx, midy+1) == false)
-										{
-											midy=midy+1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-										if(p.contains(midx+1, midy)  == false)
-										{
-											midx=midx+1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-									
-									midx=midx+2;
-									midy=midy+5;
-									
-								}
-								else if(midx<xtest && midy>ytest)
-								{
-									while(Math.abs(midx-xtest)>2 || Math.abs(midy-ytest)>2)
-									{
-									while(true)
-									{
-										if(p.contains(midx+1, midy) == false)
-										{
-											midx=midx+1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-										if(p.contains(midx, midy-1)  == false)
-										{
-											midy=midy-1;
-										}
-										else
-										{
-											break;
-										}
-
-									}
-									
-									midx=midx+3;
-									midy=midy-3;
-									
-								}
-								intersect = Line2D.linesIntersect(midx,midy,setx[i],sety[i],setx[h],sety[h],setx[h+1],sety[h+1]);
-							}
-						}
-					}
-				}
-				//if intersecting, don't add line
-				if(intersect == true)
-				{
-					break;
-				}
+				length++;
+				endx = setx[test] + length*Math.cos(ang);
+				endy = sety[test] + length*Math.sin(ang);
 			}
-			//if not intersecting, do add line
-			if(intersect == false)
-			{
-				sightx[count] = setx[i]/5;
-				sighty[count] = sety[i]/5;
-				count++;
-			}
-			intersect = false;
-			
+			g.drawLine((int)setx[test], (int)sety[test], (int)endx, (int)endy);
+			ang=ang+.001;
 		}
-		for(int i  = 0; i<count;i++)
-		{
-			System.out.print("("+sightx[i]+","+sighty[i]+")");
-			g.drawLine((((int)setx[test])), (((int)sety[test])), (((int)sightx[i]*5)),(((int)sighty[i]*5)));
-		}
-		//g.setColor(Color.GREEN);
-		//g.fillPolygon(sightx, sighty, count);
-		count = 0;
 	}
 	public static void main(String[] args) throws IOException
 	{
